d3d8a3d4d7b37f2d44fb04553e369fbef62943ca
redaabou <aboulouafareda@gmail.com>
2024-06-07 13:06:18 +0100

    node-cron added

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@
 
 import dotenv from "dotenv";
 import app from "./app";
+import cron from 'node-cron';
+import { sendMailToAllSubs } from "./controllers/newsletter-controller";
 dotenv.config();
 
 import connectDB from "./config/db";
@@ -11,6 +13,11 @@ async function startServer() {
   try {
     await connectDB(); // Ensure the database connection is established before starting the server
     const PORT = process.env.PORT || 3000;
+    
+    // Schedule task to run every friday at 12:00
+    cron.schedule('0 12 * * 5', async () => {
+      await sendMailToAllSubs(null, null);
+    });
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
